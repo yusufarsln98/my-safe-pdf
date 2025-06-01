7d63d2fc891fe33c7751a018197ab5049cc9d66f
Yusuf Arslan <yusufarsln98@outlook.com>
2025-06-01 12:24:28 +0300

    feat: make sider closable

--- src/layout/page-layout/PageLayout.tsx
+++ src/layout/page-layout/PageLayout.tsx
@@ -1,8 +1,9 @@
-import { Layout } from 'antd'
-import React from 'react'
+import { Layout, Grid } from 'antd'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 
 const { Content, Sider } = Layout
+const { useBreakpoint } = Grid
 
 export const Container = styled(Layout)`
 	min-height: calc(100vh - 64px);
@@ -15,12 +16,19 @@ export const StyledContent = styled(Content)`
 	flex: 1;
 `
 
-export const StyledSider = styled(Sider)`
-	min-height: calc(100vh - 64px);
+export const StyledSider = styled(Sider)<{ $collapsed?: boolean }>`
+	height: ${(props) => (props.$collapsed ? '0' : 'calc(100vh - 64px)')};
 	background-color: ${(props) => props.theme.colorBgContainer};
 	border-left: ${(props) => props.theme.colorBorder} 1px solid;
 	display: flex;
 	flex-direction: column;
+
+	.ant-layout-sider-zero-width-trigger {
+		top: 12px;
+		background: ${(props) => props.theme.colorBgContainer};
+		color: ${(props) => props.theme.colorText};
+		border: 1px solid ${(props) => props.theme.colorBorder};
+	}
 `
 
 export const SidebarContent = styled.div`
@@ -40,17 +48,40 @@ export interface PageLayoutProps {
 	children: React.ReactNode
 	sider?: React.ReactNode
 	siderWidth?: number
+	collapsedWidth?: number
 }
 
 export const PageLayout: React.FC<PageLayoutProps> = ({
 	children,
 	sider,
 	siderWidth = 300,
+	collapsedWidth = 0,
 }) => {
+	const [collapsed, setCollapsed] = useState(false)
+	const screens = useBreakpoint()
+
+	// Automatically collapse on small screens
+	useEffect(() => {
+		setCollapsed(!screens.md)
+	}, [screens.md])
+
 	return (
 		<Container>
 			<StyledContent>{children}</StyledContent>
-			{sider && <StyledSider width={siderWidth}>{sider}</StyledSider>}
+			{sider && (
+				<StyledSider
+					width={siderWidth}
+					collapsible
+					collapsed={collapsed}
+					onCollapse={setCollapsed}
+					collapsedWidth={collapsedWidth}
+					breakpoint='md'
+					reverseArrow
+					$collapsed={collapsed}
+				>
+					{sider}
+				</StyledSider>
+			)}
 		</Container>
 	)
 }
